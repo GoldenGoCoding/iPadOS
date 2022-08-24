203feb1b65dcf544207c48dcd1dae25219dc496f
GoldenGo <101265437+GoldenGoCoding@users.noreply.github.com>
2022-08-24 22:06:13 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 var CC = document.getElementById('controlCenter').style;
-var CoCe = document.getElementById('CC').style;
+var CoCe = document.getElementById('CC');
 var trig = document.getElementById('control-center-trig').style;
 var icon1 = document.getElementById('icon1');
 var icon2 = document.getElementById('icon2').style;
@@ -11,6 +11,18 @@ var batteryDisplay = document.getElementById('bD');
 var batteryDisplay2 = document.getElementById('battery').style;
 var appGrid = document.getElementById('table').style;
 var height = window.innerHeight;
+var app = document.getElementById('app');
+
+// apps source code goes here!!
+var findMy = `
+<h1>Find My!!</h1>
+`;
+var files = `
+<h1>Files!!</h1>
+`;
+var notFound = `
+<h1>Sorry this app is empty</h1>
+`;
 
 var batteryPercentage = 100;
 var wifiSignal = 2;
@@ -19,25 +31,46 @@ var wifiSignal = 2;
 
 function openCC() {
     CC.top = '0';
-    CoCe.opacity = '1';
-    CoCe.zIndex = '0';
-    CoCe.backdropFilter = 'blur(10px)';
+    CoCe.style.opacity = '1';
+    CoCe.style.zIndex = '0';
+    CoCe.style.backdropFilter = 'blur(10px)';
     body.transform = 'scale(0.9)';
-    dock.transform = 'scale(0.8)';
-    trig.margin = '15px 20px';
+    dock.transform = 'scale(0.9)';
+    app.transform = 'scale(0.9)';
+    trig.padding = '20px 20px';
+    trig.width = '275px';
     batteryDisplay.style.fontWeight = '900';
     batteryDisplay.style.fontSize = '15px';
     icon1.style.height = '30px';
     icon2.transform = 'scale(1.4)';
     icon2.margin = '6px';
 }
+CoCe.addEventListener('wheel', async function () {
+    await delay(100);
+    if (document.getElementById('top').getBoundingClientRect().y < 0) {
+        trig.backdropFilter = 'blur(20px)';
+        trig.borderBottom = '1px #fff2 solid';
+        trig.borderRadius = '0px';
+    }
+    else {
+        trig.backdropFilter = '';
+        trig.borderBottom = '';
+        trig.borderRadius = '4px';
+    }
+});
 function closeCC() {
     CC.top = '-100%';
-    CoCe.opacity = '0';
-    CoCe.zIndex = '-1';
+    CoCe.style.opacity = '0';
+    CoCe.style.zIndex = '-1';
     body.transform = 'scale(1)';
     dock.transform = 'scale(1)';
+    app.transform = 'scale(1)';
+    trig.padding = '0';
     trig.margin = '0';
+    trig.width = '';
+    trig.backdropFilter = '';
+    trig.borderBottom = '';
+    trig.borderRadius = '4px';
     batteryDisplay.style.fontWeight = '500';
     batteryDisplay.style.fontSize = '11px';
     icon1.style.height = '20px';
@@ -45,61 +78,79 @@ function closeCC() {
     icon2.margin = '2px';
 }
 
-navigator.getBattery().then(function(battery) {
-    battery.addEventListener('levelchange', function() {    
-      // Do stuff when the level changes, you can get it
-
-      batteryPercentage = Math.round(battery.level*100);
-      batteryDisplay.innerHTML = `${batteryPercentage}%`;      // from battery.level
-      batteryDisplay2.width = `${batteryPercentage}%`;
-      icon1.src = `wifi ${wifiSignal}.svg`;
+navigator.getBattery().then(function (battery) {
+    battery.addEventListener('levelchange', function () {
+        batteryPercentage = Math.round(battery.level * 100);
+        batteryDisplay.innerHTML = `${batteryPercentage}%`;
+        batteryDisplay2.width = `${batteryPercentage}%`;
+        icon1.src = `wifi ${wifiSignal}.svg`;
     })
-    batteryPercentage = Math.round(battery.level*100);
+    batteryPercentage = Math.round(battery.level * 100);
     batteryDisplay.innerHTML = `${batteryPercentage}%`;
     batteryDisplay2.width = `${batteryPercentage}%`;
     icon1.src = `wifi ${wifiSignal}.svg`;
 });
 
-var app = document.getElementById('app').style;
 
-appGrid.height = `${height - parseFloat(dock.height)}px`;
+document.querySelectorAll('img').forEach(function (a, key) {
+    if (a == icon1) { } else {
 
-document.querySelectorAll('img').forEach(function(a, key) {
-    a.addEventListener('click', async function() {
-        var rect = a.getBoundingClientRect();
-        app.display = 'block';
-        app.transition = '0.4s cubic-bezier(0, 0, 0, 1)';
-        app.position = 'fixed';
-        app.height = '48px';
-        app.width = '48px';
-        app.borderRadius = '12px';
-        app.top = `${rect.y}px`;
-        app.left = `${rect.x}px`;
-        await delay();
-        app.height = '100%';
-        app.width = '100%';
-        app.borderRadius = '20px';
-        app.top = `0`;
-        app.left = `0`;
-        app.transition = '0';
-        dock.bottom = '-100px';
-        homeBar.style.bottom = '10px';
-        homeBar.addEventListener('click', async function() {
-            app.transition = '0.4s cubic-bezier(0, 0, 0, 1)';
-            app.position = 'fixed';
+
+        a.addEventListener('click', async function () {
+
+            var rect = a.getBoundingClientRect();
+            app.style.display = 'block';
+            app.style.transition = '0.4s cubic-bezier(0, 0, 0, 1)';
+            app.style.position = 'fixed';
+            app.style.height = '48px';
+            app.style.width = '48px';
+            app.style.borderRadius = '12px';
+            app.style.top = `${rect.y}px`;
+            app.style.left = `${rect.x}px`;
             await delay();
-            app.height = '48px';
-            app.width = '48px';
-            app.borderRadius = '12px';
-            app.top = `${rect.y}px`;
-            app.left = `${rect.x}px`;
-            app.transition = '0';
-            dock.bottom = '15px';
-            homeBar.style.bottom = '-10px';
-            await delay(500);
-            app.display = 'none';
+            app.style.height = '100%';
+            app.style.width = '100%';
+            app.style.borderRadius = '20px';
+            app.style.top = `0`;
+            app.style.left = `0`;
+            app.style.transition = '0';
+            dock.bottom = '-100px';
+            homeBar.style.bottom = '5px';
+
+
+            homeBar.addEventListener('mouseover', async function () {
+                await delay(500);
+                dock.bottom = '15px';
+                homeBar.onmouseleave = async function () {
+                    await delay(1000);
+                    dock.bottom = '-100px';
+                };
+                homeBar.onclick = async function () {
+                    homeBar.onmouseleave = null;
+                    app.style.transition = '0.4s cubic-bezier(0, 0, 0, 1)';
+                    app.style.position = 'fixed';
+                    await delay();
+                    closeCC();
+                    app.style.height = '48px';
+                    app.style.width = '48px';
+                    app.style.borderRadius = '12px';
+                    app.style.top = `${rect.y}px`;
+                    app.style.left = `${rect.x}px`;
+                    app.style.transition = '0';
+                    dock.bottom = '15px';
+                    homeBar.style.bottom = '-10px';
+                    await delay(360);
+                    app.style.display = 'none';
+                };
+            });
+            if (a.src === './36709/ios-icons/svg/files-2365229.svg') {
+                app.innerHTML = files;
+            }
+            if (a.src === './36709/ios-icons/svg/find-my-1575949.svg') {
+                app.innerHTML = findMy;
+            }
         });
-    });
+    }
 });
 function delay(milliseconds) {
     return new Promise(resolve => setTimeout(resolve, milliseconds));
